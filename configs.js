a06437222f13faf3619d27bb13f555603f6d816a
Administrator <zkunsang@illuni.com>
2021-01-18 22:49:28 +0900

    fluent bug fix

--- configs.js
+++ configs.js
@@ -1,7 +1,6 @@
 const nodeEnv = process.env.NODE_ENV;
 
 const dbMongoLog = require(`./configs/${nodeEnv}/dbMongoLog.json`);
-const fluent = require(`./configs/${nodeEnv}/fluent.json`);
 const App = require(`./configs/${nodeEnv}/app.json`);
 
 module.exports = {
